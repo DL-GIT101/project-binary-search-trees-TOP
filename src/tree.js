9c42f4835006c39608b2913369cb913d4e6e1fad
donlHenard <pastor.git101@gmail.com>
2024-04-04 15:47:39 +0800

    add ccreate tree buildtree fn has only array methods first

---
+++ src/tree.js
@@ -0,0 +1,17 @@
+const CreateTree = (array) => {
+
+    let _root = null;
+
+    function buildTree(array) {
+
+        array.sort((a, b) => a - b);
+        console.log(array);
+        
+       // return rootNode;
+    }
+
+    buildTree(array);
+
+}
+
+export { CreateTree };
